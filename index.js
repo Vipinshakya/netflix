4fe7ee139dfca5a63ee33818239aa37a55c09796
Vipinshakya <111337665+Vipinshakya@users.noreply.github.com>
2022-09-24 12:56:43 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const arrow = document.querySelectorAll('arrow');
+const movielist= document.querySelectorAll('movie-list')
+const menulist=document.querySelectorAll('menulist')
+
+arrow.forEach((arrow,i) => {
+   console.log(arrow)
+});
+ function (menulist) {
+  onclick() 
+} 
+console.log(list)
